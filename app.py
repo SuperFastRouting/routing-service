591aa77d796268a4634e1f70154f826480381921
ccchoi2 <ccchoi@ualberta.ca>
2024-02-15 16:08:23 -0700

    project init

---
+++ app.py
@@ -0,0 +1,15 @@
+"""
+A simple flask app
+"""
+
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello There!'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
